c678967a36b096130cd2d1c4c8f2ccfef69cef3a
Ibraheem0SID <ibraheem88saadi88@gmail.com>
2023-05-23 18:55:02 +0300

    updating enums

--- lib/util/enums.dart
+++ lib/util/enums.dart
@@ -1,47 +1,89 @@
-enum UserRole  {
+enum UserRole {
   superAdmin,
-  readOnly,
   readWrite,
-  }
-  
-  //Provider Social Status 
-enum ProviderSS { 
-  widow,
-	divorced,
-	married,
-	specialNeeds,
-	missing,
-	hanging,
-  }
+  readOnly;
+}
 
+//Provider Social Status
+enum ProviderSS {
+  widow(1, 'ارملة'),
+  divorced(2, 'مطلقة'),
+  married(3, 'متزوجة'),
+  specialNeeds(4, 'احتياجات خاصة'),
+  missing(5, 'الزوج مفقود'),
+  hanging(6, 'معلقة');
 
-enum FamilyType{
-  specialNeeds,
-	orphans,
-	chase,
-	missing,
-	noProvider,
-	others,
+  final int number;
+  final String value;
+  const ProviderSS(this.number, this.value);
+
+  static ProviderSS getSS(int number) =>
+      ProviderSS.values.firstWhere((ss) => ss.number == number);
+
+  int toInt() => number;
+}
+
+enum FamilyType {
+  specialNeeds(1, 'مرضى'),
+  orphans(2, 'ايتام'),
+  chase(3, 'متعففين'),
+  missing(4, 'مفقود م'),
+  noProvider(5, 'بلا معيل'),
+  others(6, 'اخرى');
+
+  final int number;
+  final String value;
+  const FamilyType(this.number, this.value);
+
+  static FamilyType getType(int number) =>
+      FamilyType.values.firstWhere((type) => type.number == number);
+
+  int toInt() => number;
 }
 
 enum IncomeType {
-  martyrs,
-	retired,
-	aid,
-	others,
-  }
+  martyrs(1, 'شهيد'),
+  retired(2, 'تقاعد'),
+  aid(3, 'رعاية'),
+  others(4, 'اخرى');
 
-  enum HousingType{
-  rent,
-	ownership,
-	illegal,
-	others,   
-  }
+  final int number;
+  final String value;
+  const IncomeType(this.number, this.value);
+
+  static IncomeType getType(int number) =>
+      IncomeType.values.firstWhere((type) => type.number == number);
+
+  int toInt() => number;
+}
+
+enum HousingType {
+  rent(1, 'إيجار'),
+  ownership(2, 'ملك'),
+  illegal(3, 'تجاوز'),
+  others(4, 'اخرى');
+
+  final int number;
+  final String value;
+  const HousingType(this.number, this.value);
+
+  static HousingType getType(int number) =>
+      HousingType.values.firstWhere((type) => type.number == number);
+
+  int toInt() => number;
+}
 
 enum FamilyStatus {
-  veryWeak,
-	weak,
-	average,
-  }
+  veryWeak(1, 'ضعيف جدا'),
+  weak(2, 'ضعيف'),
+  average(3, 'متوسط');
 
+  final int number;
+  final String value;
+  const FamilyStatus(this.number, this.value);
 
+  static FamilyStatus getStatus(int number) =>
+      FamilyStatus.values.firstWhere((status) => status.number == number);
+
+  int toInt() => number;
+}
