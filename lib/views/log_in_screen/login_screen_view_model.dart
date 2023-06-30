e1022065417a3d2d8130d0678c21dbe56edd89fb
Ibraheem0SID <ibraheem88saadi88@gmail.com>
2023-07-01 02:05:06 +0300

    update login VM

--- lib/views/log_in_screen/login_screen_view_model.dart
+++ lib/views/log_in_screen/login_screen_view_model.dart
@@ -1,56 +1,51 @@
-import 'package:flutter/cupertino.dart';
+import 'package:charity_org/util/api_helper.dart';
 import 'package:flutter/material.dart';
 
+import '../../util/local_storage.dart';
 import '../homeScreen/home_screen.dart';
 
 class LogInScreenModelView {
-  late String user;
+  late String email;
   late String password;
   final GlobalKey<FormState> formKey = GlobalKey<FormState>();
-  Text welcome = const  Text("  مرحبا بك ",
-      style: TextStyle(
-          fontSize: 30,
-          fontWeight: FontWeight.bold));
+  ApiBase api = ApiBase();
+
+  String emailText = " اسم المستخدم ";
+  String passwordText = " كلمة المرور ";
+  String logInText = " تسجيل الدخول ";
+  Text welcome = const Text("  مرحبا بك ",
+      style: TextStyle(fontSize: 30, fontWeight: FontWeight.bold));
   Icon welcomeIcon = Icon(
     Icons.waving_hand,
     size: 40,
     color: Colors.yellow.shade600,
   );
-  String userText=" user name  ";
-  String passwordText = " password ";
-  String logInText = " Log in ";
-  String userEmpty = " Please enter your user name  ";
-  String userValid = " Please enter a valid user name ";
-  String passwordValid = " Please enter a valid password ";
-  String passwordEmpty = " Please enter your password ";
+  String userText = " إسم المستخدم ";
 
   TextStyle textStyle = const TextStyle(fontSize: 16, color: Colors.white);
 
-  final RegExp userRegExp = RegExp( r'^[a-zA-Z0-9]+$');
-  final RegExp passwordRegExp =
-      RegExp(r'^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{8,}$');
-
+  final RegExp emailRegExp = RegExp(r"^[a-zA-Z0-9_\-=@,\.]+$");
 
   BorderRadius borderRadius = BorderRadius.circular(20);
 
   String? emailValidator(String? value) {
     if (value!.isEmpty) {
-      return userEmpty;
-    } else if (!userRegExp.hasMatch(value)) {
-      return userValid;
+      return 'يجب ادخال اسم المستخدم';
+    } else if (!emailRegExp.hasMatch(value)) {
+      return "تحتوي كلمة المرور على رموز غير مقبولة";
     }
     return null;
   }
 
   onSavedEmail(String? value) {
-    user = value!;
+    email = value!;
   }
 
   String? passwordValidator(value) {
     if (value!.isEmpty) {
-      return passwordEmpty;
-    } else if (!passwordRegExp.hasMatch(value)) {
-      return passwordValid;
+      return "يرجى إدخال كلمة المرور";
+    } else if (value.length < 8) {
+      return "كلمة المرور قصيرة";
     }
     return null;
   }
@@ -60,10 +55,15 @@ class LogInScreenModelView {
   }
 
   logIn(BuildContext context) async {
-    bool isWaiting = true;
-    await Future.delayed(const Duration(seconds: 2));
     if (formKey.currentState!.validate()) {
       formKey.currentState!.save();
+      dynamic response = await api.post(
+          url: 'auth/login',
+          body: {'username': email, 'password': password},
+          containHeaders: false);
+      Prefs().setUserId(response["user_id"]);
+      Prefs().setToken(response["token"]);
+      print(Prefs().token);
       Navigator.push(
           context, MaterialPageRoute(builder: (context) => const HomeScreen()));
       // Do something with the user's input
